c654245a3f9182cbadb68691f5d92b6b9743169d
Max <maxnelson997@gmail.com>
2017-12-26 18:37:01 -0700

    implemented new follower cloud function notification

--- functions/index.js
+++ functions/index.js
@@ -11,36 +11,68 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
+//listen for following events and then trigger a push notification
+exports.observeFollowing = functions.database.ref('/following/{uid}/{followingId}')
+	.onCreate(event => {
+		//lets log out some messages
+		var uid = event.params.uid
+		var followingId = event.params.followingId
+		console.log('User: ' + uid + ' is following: ' + followingId)
+
+		//snatch fcmToken to send a pushnoti
+		return admin.database().ref('/users/' + followingId).once('value', snapshot => {
+			var userWeIsFollowing = snapshot.val()
+
+			return admin.database().ref('/users/' + uid).once('value', snapshot => {
+				var userDoingTheFollowing = snapshot.val()
+
+				var payload = {
+					notification: { 
+						title: 'new homie!',
+						body: userDoingTheFollowing.username + ' is now following u fam!'
+					}
+				}
+
+				admin.messaging().sendToDevice(userWeIsFollowing.fcmToken, payload)
+					.then(function(response) {
+						console.log("Successfully sent message:", response);
+					})
+					.catch(function(error) {
+						console.log("Error sending message:", error);
+					});
+			})
+		})
+	})
+
+
 
 exports.sendPushNotifications = functions.https.onRequest((req, res) => {
 	res.send("Attempting to send push notification..")
 	console.log("LOGGER --- Trying to send push message....")
 
-	// This registration token comes from the client FCM SDKs.
-	var fcmToken = "fb_YltK0us8:APA91bFrsN1BZ4CshA7UzZsVayOx1bngq_hzdFahovZ6IhTovH3tSWORi9yiGJoENH9lfjX9ERb3rAmOPhR2dv6c7DlCjCuE5XOnwhUx8OAYCofaj8S1iRvlt3TXe3S9genan0kl_U_O";
+	var uid = '7qWdwZKyWFUCo6qtKUJVV0wNv0z2'
 
-	// See the "Defining the message payload" section below for details
-	// on how to define a message payload.
-	var payload = {
-	  notification: { 
-	  	title: "Push notification TITLE HERE BOI",
-	  	body: "Body over here is our message body"
-	  },
-	  data: {
-	    score: "850",
-	    time: "2:45"
-	  }
-	};
+	return admin.database().ref('/users/' + uid).once('value', snapshot => {
+		var user = snapshot.val()
+		console.log('User username: ' + user.username + 'fcmToken: ' + user.fcmToken)
 
-	// Send a message to the device corresponding to the provided
-	// registration token.
-	admin.messaging().sendToDevice(fcmToken, payload)
-	  .then(function(response) {
-	    // See the MessagingDevicesResponse reference documentation for
-	    // the contents of response.
-	    console.log("Successfully sent message:", response);
-	  })
-	  .catch(function(error) {
-	    console.log("Error sending message:", error);
-	  });
-})
+		var payload = {
+			notification: { 
+				title: "Push notification TITLE HERE BOI",
+				body: "Body over here is our message body"
+			}
+		};
+	
+		// Send a message to the device corresponding to the provided
+		// registration token.
+		admin.messaging().sendToDevice(user.fcmToken, payload)
+			.then(function(response) {
+				// See the MessagingDevicesResponse reference documentation for
+				// the contents of response.
+				console.log("Successfully sent message:", response);
+			})
+			.catch(function(error) {
+				console.log("Error sending message:", error);
+			});
+	});
+});
